ca0735f3aceebbb02f4da902dd0d68b5e7b00cdf
bhawna <bhawnashirsh1999@gmail.com>
2024-07-24 00:14:35 +0530

    add certificate single page

---
+++ certificate-single.php
@@ -0,0 +1,98 @@
+<?php include ('include/header.php'); ?>
+<!-- Breadcrumb section -->
+<section class="section-space bg-image breadcrumb-overlay position-relative"
+    style="background-image: url('assets/images/certificate-single.webp')">
+    <div class="container position-relative z-1 py-md-5 py-0">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="breadcrumb-area">
+                    <h1 class="h1 mb-2 text-white">Certificate Name</h1>
+                    <nav aria-label="breadcrumb">
+                        <ol class="breadcrumb">
+                            <li class="breadcrumb-item text-white"><a href="./" class="text-white">Home</a></li>
+                            <li class="breadcrumb-item active text-white" aria-current="page">Certificate Name</li>
+                        </ol>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- Breadcrumb section end -->
+<section class="section-space">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-8">
+                <h4 class="fw-semibold theme-primary">ISO 9001:2015</h4>
+                <h2 class="certificate-h fw-bold text-dark mb-4">Quality management system</h2>
+                <p>ISO 9001 was first published in 1987 by the International Organization for Standardization (ISO). ISO
+                    9001 is the international standard that specifies requirement for a quality management system in any
+                    organization. It can be used to demonstrate that an organization can consistently produce goods or
+                    services of high quality through improvement, analysis, reduction and prevention of associated
+                    risks. It is the one of the most popular standard in the ISO 9000 certification series.</p>
+                <p>ISO 9001 is a methodological framework that ensures an organization’s procedures surrounding quality
+                    management meet the highest standards. This internationally recognized standard has five major
+                    requirements that cover a number of key areas, including: Product Quality, Product Delivery,
+                    Customer Satisfaction, Internal Processes and Systems.
+                </p>
+                <p>
+                    Specifying the requirements of ISO 9001 certification is a great idea for any company looking to
+                    boost credibility amongst existing customers. In some instances or in some industries, certification
+                    is required or legally mandated. The certification process includes implementing the requirements of
+                    ISO 9001:2015 and finally achieving registrar’s confirmation that the organization meets those
+                    requirements.</p>
+                <h3 class="mt-4 text-dark">Quality Management principles:</h3>
+
+                <ul class="certificate-pointers mt-4 mb-4">
+                    <li>Process Approach</li>
+                    <li>Continual improvement</li>
+                    <li>Evidence based decision making</li>
+                    <li>Relationship management</li>
+                    <li>Engagement of people</li>
+                    <li>Customer focus</li>
+                    <li>Leadership</li>
+                </ul>
+                <p>ISO 9001:2015 sets the requirements of top management in respect of the organisation’s quality policy.</p>
+                <p>Top management needs to establish a quality policy. It must be set up around what kind of environment it is trying to create, and most importantly, it has to support and align with the top management’s strategic direction in order for it to work effectively.</p>
+                <h3 class="mt-4 text-dark">Structure of ISO 9001:2015</h3>
+                <p>ISO 9001:2015 is based on Annex SL – the high-level structure. Organizations need a common language and compliance strategy for achieving and maintaining corporate objectives and compliance with corporate responsibility (CR) guidelines. Organizations need to incorporate their quality management system (QMS) into their core business processes and institute employee awareness to help drive change through the entire organization.</p>
+                <p>Plan-Do-Check-Act (PDCA) cycle applicable to all processes and the quality management system as a whole. The reason for the change is to adopt the common approach, the new document that all ISO management system standards, including ISO 9001, ISO 14001, and the recently released ISO 27001. All new management system standards will have this common structure.</p>
+                <h3 class="mt-4 text-dark">Quality Management principles:</h3>
+                <p>ISO 9001 specifies the requirements for an effective quality management system, organizations find that using the standard helps them:</p>
+                <ul class="certificate-pointers mt-4 mb-4"><li>To organize a Quality management System.</li><li>Improves customer satisfaction and employees</li><li>Cost saving</li><li>Continual improvement in the process and quality of the products.</li><li>Reach to new markets.</li><li>Identification and address process risk.</li><li>New business opportunities</li><li>Easy to integrate with the process.</li><li>Boost organizational Efficiency.</li></ul>
+                <p> ISO 9001:2015 certification means that an organization committed to the following:</p>
+                <ul class="certificate-pointers mt-4 mb-4"><li>Organization follows all the guidelines of the ISO 9001 standard.</li><li>Organization fulfils its own requirements.</li><li>Meets customer requirements and statutory and regulatory requirements.</li><li>Maintains documentation.</li></ul>
+                <p>ISO 9001 can be integrated into other Quality Management Systems (QMS) like HACCP (Hazard Analysis Critical Control Points) which is something you should consider when establishing your own QMS.</p>
+                <p>Shared High-level Structure of ISO 9001 certification requirements with other ISO management systems including ISO 14001 (Environmental) and ISO 45001 (Occupational Health & Safety) makes it easy to manage your QHSE management risks holistically.</p>
+                <h3  class="mt-4 text-dark">In addition risk based thinking being a new ISO 9001:2015 standard requirement</h3>
+                <p>A risk management danger can be defined as a potential or perceived threat that could result in failure of the project necessary to complete the task. One key takeaway from this is that effective danger management can contribute to an organization’s ability to succeed. Free ISO 9001 2015 download hints are available on SQM Publishing. A useful and effective approach to risk assessment for a quality management system requirement can help identify and evaluate the potential and existing risks and evaluate the options step by step resulting in deciding whether we should we accept, mitigate, eliminate or transfer them while also identifying the controls we need to ensure the effectiveness of our quality management system. By including ‘potential’ threats, one can put in place preventive actions. As per ISO 9001:2015 certification, quality objectives are becoming increasingly important because they have an impact on customer satisfaction and directly relate to the success of a company’s business.</p>
+            </div>
+            <div class="col-lg-4">
+                <div class="other-certificate">
+                    <div class="certificate-head"> <h3 class="text-white">Other Certificates</h3></div>
+                   
+                    <ul>
+                        <li><a href=""><i class="ri-arrow-right-double-fill"></i> ISO 9001:2015</a></li>
+                        <li><a href=""><i class="ri-arrow-right-double-fill"></i> ISO 10002:2014</a></li>
+                        <li><a href=""><i class="ri-arrow-right-double-fill"></i> ISO 9001:2015</a></li>
+                        <li><a href=""><i class="ri-arrow-right-double-fill"></i> ISO 9001:2015</a></li>
+                        <li><a href=""><i class="ri-arrow-right-double-fill"></i> ISO 9001:2015</a></li>
+                        <li><a href=""><i class="ri-arrow-right-double-fill"></i> ISO 9001:2015</a></li>
+                        <li><a href=""><i class="ri-arrow-right-double-fill"></i> ISO 9001:2015</a></li>
+                        <li><a href=""><i class="ri-arrow-right-double-fill"></i> ISO 9001:2015</a></li>
+                        <li><a href=""><i class="ri-arrow-right-double-fill"></i> ISO 9001:2015</a></li>
+                        <li><a href=""><i class="ri-arrow-right-double-fill"></i> ISO 9001:2015</a></li>
+                        <li><a href=""><i class="ri-arrow-right-double-fill"></i> ISO 9001:2015</a></li>
+                        <li><a href=""><i class="ri-arrow-right-double-fill"></i> ISO 9001:2015</a></li>
+                        <li><a href=""><i class="ri-arrow-right-double-fill"></i> ISO 9001:2015</a></li>
+                        <li><a href=""><i class="ri-arrow-right-double-fill"></i> ISO 9001:2015</a></li>
+                        <li><a href=""><i class="ri-arrow-right-double-fill"></i> ISO 9001:2015</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</section>
+
+<?php include ('include/footer.php'); ?>
