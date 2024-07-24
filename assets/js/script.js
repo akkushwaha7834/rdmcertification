27ac3d7058362b12f8f3cf2356cf4eebb59ea53b
bhawna <bhawnashirsh1999@gmail.com>
2024-07-24 23:19:11 +0530

    add it service detail page

--- assets/js/script.js
+++ assets/js/script.js
@@ -73,13 +73,33 @@
     spaceBetween: 50,
     slidesPerView: '3',    
     speed:3000,
-    spaceBetween: 10,
-    // loop:true,
-    // autoplay: {
-    //   delay: 0,
-    //   disableOnInteraction: false,
-    // },
-
+    loop:true,
+    autoplay: {
+      delay: 0,
+      disableOnInteraction: false,
+    },
+    // Responsive breakpoints
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 20
+    },
+    // when window width is >= 480px
+    480: {
+      slidesPerView: 2,
+      spaceBetween: 20
+    },
+    // when window width is >= 640px
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 20
+    },
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 20
+    },
+  }
   });
   // Odometer JS
 	// $(".odometer").each((function(t){$(".odometer").each((function(){var t=$(this).attr("data-count");$(this).html(t)}))}));
